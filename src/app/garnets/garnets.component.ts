3078512e373dda1331fbbd59a077004e5f99ee5c
paul <pxl34232@gmail.com>
2020-05-24 14:58:58 -0400

    garnet basics and some css

--- src/app/garnets/garnets.component.ts
+++ src/app/garnets/garnets.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-garnets',
   templateUrl: './garnets.component.html',
-  styleUrls: ['./garnets.component.scss']
+  styleUrls: ['./garnets.component.scss'],
 })
 export class GarnetsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
